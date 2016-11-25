d4d5cdf2b0ce98c25295bb0ce16cfdaf8936f618
victor0402 <vcarvalho0402@gmail.com>
2016-11-25 16:36:35 -0200

    cloning components before move them

--- example/demo.js
+++ example/demo.js
@@ -1,45 +1,44 @@
-// target elements with the "draggable" class
-interact('.pbjs-component')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
-    restrict: {
-      restriction: document.getElementById('pbjs-target-container'),
-      endOnly: true,
-      elementRect: {top: 0, left: 0, bottom: 1, right: 1}
-    },
-    // enable autoScroll
-    autoScroll: true,
+interact('.pbjs-component').draggable({
+  restrict: {
+    restriction: $('#pbjs-target-container')[0],
+    elementRect: {top: 0, left: 0, bottom: 1, right: 1},
+    endOnly: true
+  },
 
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-    // call this function on every dragend event
-    onend: function (event) {
-      var textEl = event.target.querySelector('p');
+  onmove: function (event) {
+    var target = event.target;
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      textEl && (textEl.textContent =
-        'moved a distance of '
-        + (Math.sqrt(event.dx * event.dx +
-          event.dy * event.dy) | 0) + 'px');
-    }
-  });
+    target.style.webkitTransform =
+      target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 
-function dragMoveListener(event) {
-  var target = event.target,
-  // keep the dragged position in the data-x/data-y attributes
-    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  },
+  onend: function (event) {
+    console.log(event);
+  }
+}).on('move', function (event) {
+  var interaction = event.interaction;
 
-  // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+  if (interaction.pointerIsDown && !interaction.interacting() && event.currentTarget.getAttribute('clonable') != 'false') {
+    var original = event.currentTarget;
+    var clone = event.currentTarget.cloneNode(true);
+    var x = clone.offsetLeft;
+    var y = clone.offsetTop;
+    clone.setAttribute('clonable', 'false');
+    clone.style.position = "absolute";
+    clone.style.left = original.offsetLeft + "px";
+    clone.style.top = original.offsetTop + "px";
+    original.parentElement.appendChild(clone);
+    interaction.start({name: 'drag'}, event.interactable, clone);
+  }
+});
 
-  // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
-}
-
-// this is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener;
+interact('#pbjs-target-container').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '.pbjs-component',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75
+});
