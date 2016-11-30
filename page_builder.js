f1d6b4b41f1cfc12292aa78615d52357208763c3
Victor <vcarvalho0402@gmail.com>
2016-11-29 22:50:25 -0200

    moving from interactjs to jquery-ui draggable plugin

--- page_builder.js
+++ page_builder.js
@@ -14,93 +14,29 @@ var PageBuilderJS = {};
   };
 
 
-  var startInteractJs = function () {
-    interact('.pbjs-component').draggable({
-      restrict: {
-        restriction: $('#pbjs-target-container')[0],
-        elementRect: {top: 0, left: 0, bottom: 1, right: 1},
-        endOnly: true
-      },
-
-      onmove: function (event) {
-        var target = event.target;
-        var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-        var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-        target.style.webkitTransform =
-          target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-      },
-      onend: function (event) {
-      }
-    }).on('move', function (event) {
-      var interaction = event.interaction;
-
-      if (interaction.pointerIsDown && !interaction.interacting() && event.currentTarget.getAttribute('clonable') != 'false') {
-        var original = event.currentTarget;
-        var clone = event.currentTarget.cloneNode(true);
-        var x = clone.offsetLeft;
-        var y = clone.offsetTop;
-        clone.setAttribute('clonable', 'false');
-        clone.style.position = "absolute";
-        clone.style.left = original.offsetLeft + "px";
-        clone.style.top = original.offsetTop + "px";
-        original.parentElement.appendChild(clone);
-        interaction.start({name: 'drag'}, event.interactable, clone);
+  var startJqueryDraggableComponents = function () {
+    $(".pbjs-dropzone").sortable({
+      revert: true,
+      placeholder: "pbjs-sortable-placeholder",
+      stop: function (event, ui) {
+        console.log(ui.item);
+        ui.item.replaceWith('<div class="pbjs-draggable-droped"></div>');
       }
     });
 
-    interact('.pbjs-dropzone').dropzone({
-      // only accept elements matching this CSS selector
-      accept: '.pbjs-component',
-      // Require a 75% element overlap for a drop to be possible
-      overlap: 0.1,
+    $(".pbjs-component").draggable({
+      connectToSortable: ".pbjs-dropzone",
+      revert: "invalid",
+      cursor: "move",
+      cursorAt: {top: 30, left: 50},
+      helper: function (event, el) {
+        console.log('event', event);
+        console.log('el', el);
 
-      ondropactivate: function (event) {
-
-        // add active dropzone feedback
-        $('.pbjs-dropzone').addClass('pbjs-drop-active');
-      },
-      ondragenter: function (event) {
-        var draggableElement = event.relatedTarget,
-          dropzoneElement = event.target;
-
-        // feedback the possibility of a drop
-        $('.pbjs-dropzone').addClass('pbjs-drop-target');
-        // dropzoneElement.classList.add('pbjs-drop-target');
-        draggableElement.textContent = 'Dragged in';
-      },
-      ondragleave: function (event) {
-        // remove the drop feedback style
-
-        $('.pbjs-dropzone').removeClass('pbjs-drop-target');
-
-        event.relatedTarget.textContent = 'Dragged out';
-      },
-      ondrop: function (event) {
-
-        event.relatedTarget.htmlContent = 'Dropped';
-
-        var type = $(event.relatedTarget).data('type');
-        var componentvalue = $(event.relatedTarget).data('componentvalue');
-        var component = COMPONENTS[type];
-
-        $(event.target).append(component.exportContent(componentvalue));
-
-        component.afterExport();
-
-        $(event.relatedTarget).fadeOut('fast', function () {
-          $(this).remove()
-        })
-
-      },
-      ondropdeactivate: function (event) {
-        // remove active dropzone feedback
-        $('.pbjs-dropzone')
-          .removeClass('pbjs-drop-active')
-          .removeClass('pbjs-drop-target');
+        var $draggable = $(event.currentTarget);
+        var component = COMPONENTS[$draggable.data('type')];
+        var width = $('#pbjs-target-container').width();
+        return $('<div class="pbjs-draggable-onmove" style="width: ' + width + 'px"></div>');
       }
     });
   };
@@ -148,8 +84,8 @@ var PageBuilderJS = {};
   pb.initialize = function (settings) {
     var options = $.extend({}, defaultSettings, (settings || {}));
     createSidebarComponents(options.components);
-    startInteractJs();
-
+    // startInteractJs();
+    startJqueryDraggableComponents();
   }
 
 }(PageBuilderJS));
